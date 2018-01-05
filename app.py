30a0e61a7860dd088e81c938c1a9ea9bb121cceb
Alexis <ali.finney@gmail.com>
2018-01-04 19:36:11 -0700

    learning python

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from random import *
+x = randint(1,3)
+
+from time import *
+
 class Human:
   health = 30
   name = ""
@@ -9,13 +14,77 @@ class Human:
 
 myHuman = Human("Hassan")
 myHuman.eat()
-print(myHuman.health)
+print("Hassan eats",myHuman.health)
 
 class Ninja(Human):
     health = 10
     def __init__(self, name):
         Human.__init__(self, name)
+    
+    def meditate(self):
+        self.health+=5
+    
+    def backstab(self, obj):
+        obj.health -= 10
 
 kevTheNinja = Ninja("Kevin")
+
 kevTheNinja.eat()
-print(kevTheNinja.health)
+print("kevin eats", kevTheNinja.health)
+
+kevTheNinja.meditate()
+print("kevin meditates", kevTheNinja.health)
+
+kevTheNinja.backstab(myHuman)
+print("Hassan's health after being stabbed", myHuman.health)
+
+class Wizard(Human):
+    health = 25
+    magicPower = 0
+    def __init__(self, name):
+        Human.__init__(self, name)
+
+    def study(self):
+        marieTheWizard.magicPower += x
+
+    def burn(self, obj):
+        # obj.health -= 1
+        burn_time = 3
+        while burn_time:
+            sleep(1)
+            obj.health -= 1 # self.magicPower
+            print("{} has {} health points".format(obj.name, obj.health))
+            # print(f"{obj.name} has {obj.health} health points")
+            burn_time -= 1
+    def fireball(self, obj):
+        obj.health -= self.magicPower
+        self.burn(obj)
+
+marieTheWizard = Wizard("Marie")
+
+marieTheWizard.study()
+print("Marie studies", marieTheWizard.health)
+
+marieTheWizard.fireball(myHuman)
+print("Hassan after being hit by a fireball", myHuman.health)
+
+class Warrior(Human):
+    health = 40
+    armor = 10
+    def __init__(self, name):
+        Human.__init__(self, name)
+    
+    def armorUp(self):
+        charlotteTheWarrior.armorUp += 5
+
+    def shieldAlly(self, obj):
+        myHuman.shieldAlly += 5
+        myHuman.health -= 5
+
+charlotteTheWarrior = Warrior("Charlotte")
+
+charlotteTheWarrior.armorUp()
+print("Charlotte armor's up", charlotteTheWarrior.health)
+
+
+
